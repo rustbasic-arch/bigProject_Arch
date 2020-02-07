5fdfeb93d253822a71e4d78857581c91f75b4374
webing <wenweiping920@163.com>
2020-02-07 17:08:54 +0800

    change drop ord

--- mainProject1/src/main.rs
+++ mainProject1/src/main.rs
@@ -21,9 +21,9 @@ impl Drop for Dog {
 
 
 
-//drop 是在离开代码块 之后执行
-//资源释放行为逻辑方向和代码业务执行的逻辑方向 相反
-fn main() {
+//0.drop 是在离开代码块 之后执行
+//1.资源释放行为逻辑方向和代码业务执行的逻辑方向 相反
+fn main11() {
     let d1 = Dog::new("2号");
     let d3 = Dog::new("3号");
     {
@@ -40,3 +40,27 @@ fn main() {
 
 }
 //drop d1
+
+
+fn main() {
+    let d2 = Dog::new("2号");
+    let d3 = Dog::new("3号");
+    {
+        let d0 = Dog::new("0号");
+        let d1 = Dog::new("1号");
+    }//drop d2
+
+// 提前drop
+
+    drop(d2);
+    drop(d3);
+
+    println!("main end");
+//result;打印
+    //1
+    //0
+    //2
+    //3
+    //main end
+
+}
